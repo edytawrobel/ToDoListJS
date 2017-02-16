f11145409c1a9957b2673c7347b1c35e10c7adaf
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-02-16 15:50:35 +0000

    extract note text from a form submit event

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ var controller = new NoteController(list);
 var view = new NoteView(list);
 controller.htmlInserter();
 controller.makeUrlChangeShowNoteForCurrentPage();
+
+
+
+document.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    console.log(document.getElementById('new-note').value);
+
+});
